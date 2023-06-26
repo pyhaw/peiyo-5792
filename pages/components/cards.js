3405a565fd59476731654d681926fb046befe768
nuyer <98694104+nuyer@users.noreply.github.com>
2023-06-26 10:52:42 +0800

    link signout to log-in page and edit ui

--- pages/components/cards.js
+++ pages/components/cards.js
@@ -7,7 +7,7 @@ export default function Cards() {
   return (
     <div class="container mx-auto my-12 flex flex-col sm:flex-row items-center justify-center gap-4 max-w-4xl">
       <div class="mx-2 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#t">
+        <a href="#">
           <Image class="rounded-t-lg" src={pic2} alt="" />
         </a>
         <div class="p-5">
@@ -18,17 +18,17 @@ export default function Cards() {
         </div>
       </div>
 
-      <div class="mx-2 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      
+        <div class="mx-2 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="./CCAlist">
           <Image class="rounded-t-lg" src={pic2} alt="" />
-        </a>
-        <div class="p-5">
-          <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Cultural</h5>
-          </a>
+              <div class="p-5">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">Cultural</h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">click here for demo</p>
         </div>
+        </a>
       </div>
+      
 
       <div class="mx-2 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
